d3449030a3277ed3c493581e5dc92cd6ff625ee7
Juan Quijano <jc_quijano@hotmail.com>
2020-06-01 19:35:20 +0200

    Añadida la creación de la función

--- infraestructura.sh
+++ infraestructura.sh
@@ -1,2 +1,3 @@
 az group create --location westeurope -n "borrame1"
 az storage account create -g borrame1 -l westeurope -n borrame1sg --sku Standard_LRS --kind StorageV2
+az functionapp create --consumption-plan-location westeurope --name borramefunction --os-type Windows --resource-group borrame1 --runtime dotnet --storage-account borrame1sg
